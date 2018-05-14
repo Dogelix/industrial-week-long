21ac682fe1c387aebdfae7d9ef7cae354f83410e
U-UNIVERSITY\100342024 <100342024@40167EA45A9E.university.ds.derby.ac.uk>
2018-05-14 16:48:42 +0100

    .

---
+++ Assets/Tech/Utilites/Laps.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laps : MonoBehaviour {
+
+    public int laps = 0;
+    public bool CanFinish = false;
+    public bool won = false;
+
+    private int playerLap = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+
+    private void Lap()
+    {
+        if (CanFinish == true)
+        {
+            laps++;
+            CanFinish = false;
+
+            if (laps >= 4)
+            {
+                won = true;
+            }
+        }
+    }
+
+    private void Trophy()
+    {
+        if (won == true)
+        {
+
+        }
+    }
+}
