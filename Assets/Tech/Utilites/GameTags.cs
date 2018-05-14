e71f0832a9c4b57a539209a1d791bb2d7012f87e
U-UNIVERSITY\100405964 <100405964@E03F491C995C.university.ds.derby.ac.uk>
2018-05-14 10:16:17 +0100

    Player Prefab

---
+++ Assets/Tech/Utilites/GameTags.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Utilites
+{
+    public class GameTags : MonoBehaviour
+    {
+        public const string Player = "Player";
+        public const string Tower = "Tower";
+    }
+}
