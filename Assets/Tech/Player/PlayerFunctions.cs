b777f5ddb7ac5e3ea089c8df6a172b418dde4375
U-UNIVERSITY\100342024 <100342024@E03F491C979D.university.ds.derby.ac.uk>
2018-05-15 14:28:35 +0100

    .

--- Assets/Tech/Player/PlayerFunctions.cs
+++ Assets/Tech/Player/PlayerFunctions.cs
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-/*TO DO in this class:
-* 1. Keep track of lap
-* 2. Keep track of last checkpoint
-* 3. Store what towers the player has
-* 4. Figure out ranking
-*/
-
-public class PlayerFunctions : MonoBehaviour
-{
-
-=======
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,5 +15,4 @@ public class PlayerFunctions : MonoBehaviour
     {
         
     }
->>>>>>> ac6410e25e6379f6b4150ada49c7b40b1d070854
 }
---
+++ Assets/Tech/Towers/ProjectileTower.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilites;
+
+public class ProjectileTower : TurretBase
+{
+    GameObject[] players;
+    [SerializeField]
+    bool isFollowing;
+    // Use this for initialization
+    void Start ()
+    {
+        players = GameObject.FindGameObjectsWithTag(GameTags.Player);
+        isFollowing = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isFollowing)
+        {
+            transform.LookAt(FollowClosestPlayer().transform, Vector3.up);
+        }
+    }
+
+    void Tick()
+    {
+        base.Tick();
+        //Do projectile for static tower
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isFollowing = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isFollowing = false;
+    }
+
+    public GameObject FollowClosestPlayer()
+    {
+
+        GameObject closest = null;
+        float temp;
+        float curDistance = Mathf.Infinity;
+        foreach (GameObject player in players)
+        {
+            temp = Vector3.Distance(player.transform.position, transform.position);
+            if (temp < curDistance)
+            {
+                closest = player;
+                curDistance = temp;
+            }
+        }
+        return closest;
+    }
+}
