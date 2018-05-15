a9db7bc3be9a8efc6404d1d661daded427e8f023
U-UNIVERSITY\100405964 <100405964@E03F491C96A2.university.ds.derby.ac.uk>
2018-05-15 15:09:05 +0100

    Tower manager done

--- Assets/Tech/World/TowerManager.cs
+++ Assets/Tech/World/TowerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utilites;
 
 public class TowerManager : MonoBehaviour
 {
@@ -19,5 +20,19 @@ public class TowerManager : MonoBehaviour
         {
             obj.GetComponent<ITower>().Tick();
         }
+    }
+
+    public void SpawnTower(GameObject tower, GamePad.Index owner)
+    {
+        GameObject temp = Instantiate(tower, _spawnLocations[Random.Range(0, _spawnLocations.Length)].transform);
+
+        temp.GetComponent<ITower>().OwnedByPlayer = owner;
+
+        _towers.Add(temp);
+    }
+
+    public void RemoveTowerFromList(GameObject tower)
+    {
+        _towers.Remove(tower);
     }
 }
