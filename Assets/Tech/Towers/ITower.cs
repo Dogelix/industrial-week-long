7a0305a92d3cd5f517c5c2319b03221067f073dc
U-UNIVERSITY\100405964 <100405964@E03F491C96A2.university.ds.derby.ac.uk>
2018-05-15 09:06:42 +0100

    The enum and interface for the towers

---
+++ Assets/Tech/Towers/ITower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilites;
+
+public interface ITower
+{
+    /// <summary>
+    /// To be used when a tower is assigned/spawed
+    /// </summary>
+    GamePad.Index OwnedByPlayer { get; set; }
+
+    /// <summary>
+    /// Returns the type of tower
+    /// </summary>
+    ETower TowerType { get; }
+
+    /// <summary>
+    /// To be called by the TowerManager, causes logic with the tower to intiate
+    /// </summary>
+    void Tick();
+}
---
+++ Assets/Tech/Utilites/GameEnums.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilites
+{
+    public enum ETower
+    {
+        Freeze,
+        StaticCannon,
+        StandardCannon,
+        Reverse
+    }
+}
