f3ee4c571d7d68075672f4853a23abc8e7a4f727
Benjamin Edwards <35980687+Benjo-Kazooie@users.noreply.github.com>
2018-05-15 10:46:49 +0100

    Added classes for towers, towers still WIP

---
+++ Assets/Tech/Towers/TurretBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utilites;
+
+public class TurretBase : MonoBehaviour, ITower
+{
+    ETower _towerType;
+    GamePad.Index _ownedByPlayer;
+    protected int _health = 8;
+
+    public GamePad.Index OwnedByPlayer
+    {
+        get
+        {
+            return _ownedByPlayer;
+        }
+        set
+        {
+            _ownedByPlayer = value;
+        }
+    }
+
+    public ETower TowerType
+    {
+        get
+        {
+            return _towerType;
+        }
+    }
+
+    public void Tick()
+    {
+        //do things
+        DecrementHealth();
+    }
+
+    void DecrementHealth()
+    {
+        _health--;
+        //Swap out sprite here
+    }
+}
