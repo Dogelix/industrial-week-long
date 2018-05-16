a6f1eb0083034bbc5b01393c0bed053ade930e0d
U-UNIVERSITY\100401863 <100401863@E03F491C995C.university.ds.derby.ac.uk>
2018-05-16 11:24:15 +0100

    RandomBoxes

--- Assets/Tech/Random Boxes/BoxManager.cs
+++ Assets/Tech/Random Boxes/BoxManager.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class BoxManager : MonoBehaviour {
 
+    GameObject temp;
+
     private void OnTriggerEnter(Collider c)
     {
-        
+        if(c.tag == "Player")
+        {
+            temp = c.gameObject;
+            temp.GetComponent<PlayerInput>();
+            Destroy(temp);
+        }
     }
 }
