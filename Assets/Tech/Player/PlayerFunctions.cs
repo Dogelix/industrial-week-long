65ebf58788f49cdb0c4af5103b12fc715fc01efc
U-UNIVERSITY\100405964 <100405964@E03F491C96A2.university.ds.derby.ac.uk>
2018-05-17 11:43:09 +0100

    :clap:U:clap:I:clap: :clap:C:clap:O:clap:M:clap:P:clap:L:clap:E:clap:T:clap:E:clap:

--- Assets/Tech/Player/PlayerFunctions.cs
+++ Assets/Tech/Player/PlayerFunctions.cs
@@ -13,6 +13,10 @@ public class PlayerFunctions : MonoBehaviour
     [SerializeField]
     private GameObject _checkPoint;
     private int _lap;
+    private int _health = 8;
+    private int _position = 1;
+
+    private IGUIController _ui;
 
     public int Lap
     {
@@ -23,6 +27,20 @@ public class PlayerFunctions : MonoBehaviour
         set
         {
             _lap = value;
+            _ui.SetLap(_lap);
+        }
+    }
+
+    public int Position
+    {
+        get
+        {
+            return _position;
+        }
+        set
+        {
+            _position = value;
+            _ui.SetPosition(_position);
         }
     }
 
@@ -34,13 +52,27 @@ public class PlayerFunctions : MonoBehaviour
         }
     }
 
+    public void ApplyDamage(int damage)
+    {
+        _health = _health - damage;
+
+        if(_health < 1)
+        {
+            Respawn();
+        }
+
+        _ui.SetHealth(_health);
+    }
+
     public void Respawn()
     {
         transform.position = _checkPoint.transform.position;
+        _health = 8;
+        _ui.SetHealth(_health);
     }
 
     private void Start()
     {
-        
+        _ui = GetComponentInChildren<IGUIController>();
     }
 }
